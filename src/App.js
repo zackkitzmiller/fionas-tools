29234bacd0e2cf45bd8a4aec4275dcc5c442daa3
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-04-24 12:28:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+
+import CreateTodo from "./components/create-todo.component"
+import EditTodo from "./components/edit-todo.component"
+import TodosList from "./components/todos-list.component"
+import DeleteTodo from "./components/delete-todo.component"
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="http://z19r.com"><span role="img">📖</span></a>
+            <Link to="/" className="navbar-brand">Fiona's At-Home Learning Todos</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/delete/:id" component={DeleteTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/delete-todo.component.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class DeleteTodo extends Component {
+  constructor(props) {
+    super(props)
+
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/todos/' + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          todo_description: response.data.todo_description,
+          todo_responsible: response.data.todo_responsible,
+          todo_priority: response.data.todo_priority,
+          todo_completed: response.data.todo_completed
+        })
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    console.log("form submitted")
+
+    axios.post('http://localhost:4000/todos/delete/' + this.props.match.params.id)
+      .then(res => console.log(res.data))
+
+    this.props.history.push('/')
+  }
+
+  render() {
+    return(
+      <div>
+        <h3 align="center">Delete Todo</h3>
+        <p>Are you sure you want to delete the following todo:</p>
+        <ul>
+          <li>Description: <strong>{this.state.todo_description}</strong></li>
+          <li>Repsonsible: <strong>{this.state.todo_responsible}</strong></li>
+          <li>Priority: <strong>{this.state.todo_priority}</strong></li>
+          <li>Completed: <strong>{this.state.todo_completed ? 'Yes' : 'No'}</strong></li>
+        </ul>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <input type="submit" value="Delete Todo" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
